b3f6fb74dca13eac0e2a5db355168f6496f5b5e1
Arth Doshi <55782346+arthdoshi33@users.noreply.github.com>
2020-04-25 12:18:04 +0000

    santoni: Rollback fingerprint blobs to stock MM

--- device.mk
+++ device.mk
@@ -31,9 +31,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.custom \
     fingerprint.msm8937 \
-    libunwind
 
 # Init scripts
 PRODUCT_PACKAGES += \
